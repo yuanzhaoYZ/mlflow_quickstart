5be7b79da982dd48ba618a06d86e364638fafbac
Antonius <antoniuswang@insightzen.com>
2019-03-25 09:15:49 +0800

    create scripts for mlflow service

---
+++ scripts/upload2s3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# backup mlflow file and upload to s3
+
+MLFILE_DIR='/home/ec2-user/mlflow_file_store/'
+S3_DIR='s3://engineering.insightzen.com/workspace/mlflow_disk/file-store/'
+LOG='/home/ec2-user/bakup/upload.log'
+set -x
+daytime=$(date "+%Y%m%d")
+bakfile=/home/ec2-user/bakup/"mlflow"$daytime".tar"
+tar -czvf $bakfile  $MLFILE_DIR
+echo $(date +%Y-%m-%d,%H:%m:%s) >> $LOG
+echo 'create bakfile '$bakfile >> $LOG
+aws s3 cp $bakfile $S3_DIR
+echo 'upload s3 succeed' >> $LOG
+set +x
