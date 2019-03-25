e6d9c14c720ff75d7a8bf9a329f7e319ad99dc81
AntoniusWang <wangwqe123@163.com>
2019-03-25 14:14:58 +0800

    Update mlflowService.sh

--- scripts/mlflowService.sh
+++ scripts/mlflowService.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 if [ $1 = 'start' ]
 then
-    mlflow server --file-store /home/ec2-user/mlflow_file_store --default-artifact-root s3://engineering.insightzen.com/workspace/mlflow_disk/artifact-root/ --host 0.0.0.0
+    mlflow server --file-store /home/ec2-user/mlflow_file_store --default-artifact-root s3://YOUR_BUCKET_HERE/workspace/mlflow_disk/artifact-root/ --host 0.0.0.0
 fi  
